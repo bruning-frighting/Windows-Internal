711e8dbaac455513cde3119cc313b632c05b2d89
Thông Tấn <ttthong315@gmail.com>
2025-05-07 18:43:08 +0700

    add fix hidden window

--- installerPython.ps1
+++ installerPython.ps1
@@ -1,5 +1,5 @@
 $pythonUrl = "https://www.python.org/ftp/python/3.11.5/python-3.11.5-amd64.exe"
 $installerPath = "$env:TEMP\python-installer.exe"
 Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/quiet PrependPath=1 Include_test=0" -Wait
+Start-Process -FilePath $installerPath -ArgumentList "/quiet PrependPath=1 Include_test=0" -Wait -NoNewWindow -WindowStyle Hidden
 Remove-Item $installerPath
